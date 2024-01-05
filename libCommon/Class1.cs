fcb18663506c50dd3080524a6abb130894825f55
developer_ken <dengbw01@outlook.com>
2024-01-05 14:12:00 +0800

    添加项目文件。

---
+++ libCommon/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace libCommon
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ libCommon/DataTypes/Vector3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libCommon.DataTypes
+{
+    public class Vector3
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        public Vector3(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+}
